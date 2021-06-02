0a693a9f52116793c324e8401fd3fbdef38176e9
Najeeb Arif <NajeebArif786@outlook.com>
2021-06-03 00:23:16 +0530

    Adding code for cahing

---
+++ api-clusters/src/test/java/com/twa/flights/api/clusters/service/CatalogServiceTest.java
@@ -0,0 +1,19 @@
+package com.twa.flights.api.clusters.service;
+
+import com.twa.flights.api.clusters.connector.CatalogConnector;
+import com.twa.flights.api.clusters.dto.CityDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@SpringBootTest
+class CatalogServiceTest {
+
+}
